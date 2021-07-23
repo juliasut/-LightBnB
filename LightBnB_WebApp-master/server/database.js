3730ce9a4d85414817f8fba4ca15fa8d89c8b747
juliasut <juliasutygina@gmail.com>
2021-07-23 00:48:39 -0600

    Refactor getAllReservations using query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -46,7 +46,7 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser = (user) => {
   pool
-    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [user.name, user.email, user.password])
+    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
     .then((res) => res.rows[0])
     .catch((err) => err.message);
 };
@@ -59,9 +59,18 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+const getAllReservations = (guest_id, limit = 10) => {
+  pool 
+    .query(`SELECT reservations.*, properties.title FROM reservations
+    JOIN properties ON properties.id = property_id
+    WHERE guest_id = $1
+    AND reservations.end_date < now()::date
+    ORDER BY start_date DESC
+    LIMIT $2;`,
+    [guest_id, limit])
+    .then((res) => res.rows)
+    .catch((err) => err.message);
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
