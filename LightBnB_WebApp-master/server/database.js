99c4752a1d0f24e5e1e7ec58036fcb5f5f3c6007
juliasut <juliasutygina@gmail.com>
2021-07-23 00:53:12 -0600

    Update getAllReservations using 1_queries all_my_reservations.sql syntax

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -61,11 +61,14 @@ exports.addUser = addUser;
  */
 const getAllReservations = (guest_id, limit = 10) => {
   pool 
-    .query(`SELECT reservations.*, properties.title FROM reservations
-    JOIN properties ON properties.id = property_id
-    WHERE guest_id = $1
+    .query(`SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
     AND reservations.end_date < now()::date
-    ORDER BY start_date DESC
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
     LIMIT $2;`,
     [guest_id, limit])
     .then((res) => res.rows)
